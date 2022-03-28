3135fda597a446b6c616388a9e9e9316eea19ae1
zaalshena <99635291+zaalshena@users.noreply.github.com>
2022-03-28 13:38:47 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,114 @@
+/* მოგესალმებით მეგობრებო ეს არის დავალება ნომერი 1. დავალების ყველა ეტაპი აღწერილია კომენტარის სახით
+ * 1) კომენტარები
+ * 2) ცვლადები
+ * 3) მონაცემთა ტიპები
+ * 4) ოპერატორები და არითმეტიკა
+ */
+
+// 1 კომენტარები
+
+/*
+  // კომენტარები გაფუჭდა თქვენი დავალებაა
+  // არასწორად დაწერილი კომენტარი ჩაასწოროთ
+  // და ასევე გამოიყენოთ თქვენი კოდის აღსაწერად
+*/
+
+// 2 ცვლადები
+
+/*
+  // მსურს ჩემი Spotify ლისტის განახლება 
+  // ამაში კი თქვენი დახმარება მჭირდება
+  // აღწერეთ თქვენი საყვარელი მომღერლის
+  // (სახელი, გვარი, ასაკი, დაბადების წელი, დაბადების ადგილი,)
+  // გაითვალისწინეთ რომ ამისთვის უნდა
+  // გამოიყენოთ მხოლოდ (const, let)
+  // (ნებისმიერი პიროვნების ჩაწერა შეგიძლიათ)
+*/
+
+// >>>> კოდი დაწერეთ აქ
+
+const Name ='ბუბა';
+const SurName = 'კიკაბიძე';
+let Age = 82;
+const Birth = 1940;
+const BirthPlace = 'საქართველო';
+
+console.log (Name)
+console.log (SurName)
+console.log (Age)
+console.log (Birth)
+console.log (BirthPlace)
+// 
+
+
+// დროა დავიწყოთ მზადება ჩვენს მომავალ პროექტზე.
+// ჩვენი დავალეებაა მოვიფიქროთ რაიმე პროდუქტთა სია
+// რომელსაც გამოვიყენებთ მომავალში.
+
+  const WeaponName = "მაუზერი";
+  const AuthrName = "ვახტანგ ყაჩაღი";
+  const CreateDate = "18 საუკუნე";
+  let ProductQuant = 9999;
+
+  console.log (WeaponName)
+  console.log (AuthrName)
+  console.log (CreateDate)
+  console.log (ProductQuant)
+  //
+
+// 3 მონაცემთა ტიპები
+
+/*
+  // აქ რაღაც შეცდომაა თქვენი დავალებაა ცვლადის გამოცხადების
+  // დროს გაასწოროთ შეცდომა;
+*/
+
+const age = "1";
+const hisName = ["bob"];
+const isHigh = "true";
+const dateOfBirth = "1995";
+
+console.log(typeof age !== "number");
+console.log(typeof hisName !== "string");
+console.log(typeof isHigh !== "boolean");
+console.log(typeof dateOfBirth !== "number")
+//
+
+// ოპერატორები არითმეტიკა
+
+console.log (10+5)
+console.log (10-5)
+console.log (10*5)
+console.log (10/5)
+console.log (10%5)
+console.log (10**5)
+
+// გამოიყენეთ ყველა ვარიანტი რომლითაც შეგვიძლია ამ ციფრის გაზრდა
+
+// >>> კოდი დაწერეთ აქ
+
+
+//
+
+let num = 5;
+let numy = 1;
+console.log (num+numy);
+
+
+
+
+// სცადეთ შეკრიბოთ განსხვავებული ტიპის მქონე 
+// მნიშვნელობები და გაიგოთ შეკრებილი მონაცემების ტიპი
+
+// >>> კოდი დაწერეთ აქ
+ let x=15;
+ let y=10;
+ let z=5;
+
+console.log (x+y)
+console.log (x+y+z)
+console.log (x-y+z)
+console.log (x*y)
+console.log(x/z)
+console.log (x**z)
+//
